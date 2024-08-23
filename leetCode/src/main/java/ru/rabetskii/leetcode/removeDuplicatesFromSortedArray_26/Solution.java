e96616c0b9f0548be63591a1ce10a17ca6706221
omnomdom <v.rabetskii@gmail.com>
2024-08-23 23:09:44 +0700

    remove duplicates from sorted array(26 problem)

---
+++ leetCode/src/main/java/ru/rabetskii/leetcode/removeDuplicatesFromSortedArray_26/Solution.java
@@ -0,0 +1,21 @@
+package ru.rabetskii.leetcode.removeDuplicatesFromSortedArray_26;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], 1);
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        return j;
+    }
+}
