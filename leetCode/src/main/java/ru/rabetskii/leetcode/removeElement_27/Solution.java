0662264150cce6f947de6f874574f1b136914c22
omnomdom <v.rabetskii@gmail.com>
2024-08-25 21:53:13 +0700

    change package name

--- leetCode/src/main/java/ru/rabetskii/leetcode/removeElement_27/Solution.java
+++ leetCode/src/main/java/ru/rabetskii/leetcode/removeElement_27/Solution.java
@@ -1,4 +1,4 @@
-package ru.rabetskii.leetcode.removeElement;
+package ru.rabetskii.leetcode.removeElement_27;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -8,20 +8,20 @@ public class Solution {
     public int removeElement(int[] nums, int val) {
         Map<Integer, Integer> map = new HashMap<>();
         int[] temp = nums.clone();
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        for (int num : nums) {
+            if (num != val) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
             }
         }
 
         int j = 0;
-        for (int i = 0; i < temp.length; i++) {
-            if (map.containsKey(temp[i])) {
-                for (int k = 0; k < map.get(temp[i]); ++k){
-                    nums[j] = temp[i];
+        for (int value : temp) {
+            if (map.containsKey(value)) {
+                for (int k = 0; k < map.get(value); ++k) {
+                    nums[j] = value;
                     ++j;
                 }
-                map.remove(temp[i]);
+                map.remove(value);
             }
         }
 
