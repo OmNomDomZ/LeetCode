ff7b387d90304a44131811f21a5ae6e5b67bb911
omnomdom <v.rabetskii@gmail.com>
2024-11-02 16:50:35 +0700

    add: add solution to problem 35 (Search Insert Position)

---
+++ leetCode/src/main/java/ru/rabetskii/leetcode/searchInsertPosition_35/Solution.java
@@ -0,0 +1,11 @@
+package ru.rabetskii.leetcode.searchInsertPosition_35;
+
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int i = 0;
+        for (; i < nums.length && target > nums[i]; i++) {
+
+        }
+        return i;
+    }
+}
