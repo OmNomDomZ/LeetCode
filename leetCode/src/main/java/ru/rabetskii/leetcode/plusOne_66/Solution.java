6e5699720104c9988df4262c7e209cb4482baed7
omnomdom <v.rabetskii@gmail.com>
2024-11-02 21:56:40 +0700

    add: add solution to problem 66 (Plus One)

---
+++ leetCode/src/main/java/ru/rabetskii/leetcode/plusOne_66/Solution.java
@@ -0,0 +1,24 @@
+package ru.rabetskii.leetcode.plusOne_66;
+
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        boolean stopped = false;
+        for (int j = digits.length - 1; j >= 0; j--) {
+            int res = digits[j] + 1;
+            digits[j] = res % 10;
+            if (res <= 9) {
+                stopped = true;
+                break;
+            }
+        }
+
+        if (!stopped) {
+            int[] result = new int[digits.length + 1];
+            System.arraycopy(result, 1, digits, 1, digits.length - 1);
+            result[0] = 1;
+            return result;
+        }else {
+            return digits;
+        }
+    }
+}
