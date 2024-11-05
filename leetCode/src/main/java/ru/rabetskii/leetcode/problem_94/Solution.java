23b8e1e3fb535f0821eb0c443b8acead901d031d
omnomdom <v.rabetskii@gmail.com>
2024-11-05 11:36:15 +0700

    add: add solution to 94 problem

---
+++ leetCode/src/main/java/ru/rabetskii/leetcode/problem_94/Solution.java
@@ -0,0 +1,21 @@
+package ru.rabetskii.leetcode.problem_94;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        traversal(root, result);
+        return result;
+    }
+
+    private void traversal(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        traversal(root.left, result);
+        result.add(root.val);
+        traversal(root.right, result);
+    }
+}
---
+++ leetCode/src/main/java/ru/rabetskii/leetcode/problem_94/TreeNode.java
@@ -0,0 +1,46 @@
+package ru.rabetskii.leetcode.problem_94;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode() {}
+    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public TreeNode buildTree(Integer[] values) {
+        if (values == null || values.length == 0) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(values[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        int i = 1;
+
+        while (i < values.length) {
+            TreeNode current = queue.poll();
+
+            if (values[i] != null) {
+                current.left = new TreeNode(values[i]);
+                queue.add(current.left);
+            }
+            i++;
+
+            if (i < values.length && values[i] != null) {
+                current.right = new TreeNode(values[i]);
+                queue.add(current.right);
+            }
+            i++;
+        }
+        return root;
+    }
+}
